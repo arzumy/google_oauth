08de44d20027074302292d16f111d379970f9632
Arzumy MD <hello@ar.zu.my>
2011-08-26 12:38:28 +0800

    we can predict the future!

--- lib/google_oauth/prediction.rb
+++ lib/google_oauth/prediction.rb
@@ -7,5 +7,10 @@ module GoogleOAuth
     def prediction_check(model)
       _get_json "https://www.googleapis.com/prediction/v1.3/training/#{URI.escape(model, '/.')}"
     end
+
+    def prediction_predict(model, *signal)
+      body = { input: { csvInstance: signal }}
+      _post_json "https://www.googleapis.com/prediction/v1.3/training/#{URI.escape(model, '/.')}/predict", {body: body.to_json}
+    end
   end
 end
