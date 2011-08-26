cebd3a2b2299fd190701a1d6699eaa00df711cba
Arzumy MD <hello@ar.zu.my>
2011-08-26 12:22:04 +0800

    added prediction check

--- lib/google_oauth/prediction.rb
+++ lib/google_oauth/prediction.rb
@@ -1,7 +1,11 @@
 module GoogleOAuth
   class Client
-    def prediction_train!(id)
-      _post_json 'https://www.googleapis.com/prediction/v1.3/training', {body: {id: id}.to_json}
+    def prediction_train!(model)
+      _post_json 'https://www.googleapis.com/prediction/v1.3/training', {body: {id: model}.to_json}
+    end
+
+    def prediction_check(model)
+      _get_json "https://www.googleapis.com/prediction/v1.3/training/#{URI.escape(model, '/.')}"
     end
   end
 end
